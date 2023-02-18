1ccbfc998a867f63533020089ddd203b6179ab9c
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2023-02-18 17:43:29 +0000

    update readme

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 # The file directory to look in to count the words
-WORD_COUNT_DIRECTORY = "/Users/chrislovejoy/Documents/My Drive/everything/10-19 Public Creation/13 Personal website/02-personal-website/_notes"
+WORD_COUNT_DIRECTORY = ""
 
 # The path to log file to append the word count logs
-LOG_FILE_PATH = "/Users/chrislovejoy/Documents/My Drive/everything/00-09 Meta/04 Self management and Productivity/Log of number of words written.md"
+LOG_FILE_PATH = ""
 
 # Whether or not to also look in sub-folders within WORD_COUNT_DIRECTORY 
 look_in_subfolders = True
