873b0448868cca4813ac1eb3a8ae94c45bead787
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2023-02-18 17:12:30 +0000

    add logging functionality

--- get_word_count.py
+++ get_word_count.py
@@ -1,7 +1,8 @@
 import os
 import config
+import datetime
 
-# (1) Get the word count across all the files of interest
+# (1) Functions for getting the word count across all the files of interest
 
 def count_words_in_directory(directory):
     word_count = 0
@@ -19,14 +20,14 @@ def count_words(filename):
         words = file.read().split()
         return len(words)
 
+# (2) Function for outputting the result into a log file, with the desired format
+
+def log_word_count(word_count):
+    date_str = datetime.date.today().strftime("%Y-%m-%d")
+    log_filename = config.LOG_FILE_PATH
+    with open(log_filename, 'a', encoding='utf-8') as log_file:
+        log_file.write(f'- {date_str}: {word_count} words\n')
+
 
 word_count = count_words_in_directory(config.WORD_COUNT_DIRECTORY)
-
-
-# (2) Output the result into a log file, with the desired format
-
-print(word_count)
-
-# TODO: Write logging functionL
-    # Open log file
-    # Append date and total word count
+log_word_count(word_count)
